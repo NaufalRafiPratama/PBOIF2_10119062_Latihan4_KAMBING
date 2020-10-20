1de723dcb43ab4c43e7505b81ca36ad44c4604f7
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-10-20 17:18:52 +0700

    Add files via upload

---
+++ PBOIF2_10119062_Latihan4_KAMBING/src/pboif2_10119062_latihan4_kambing/PBOIF2_10119062_Latihan4_KAMBING.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119062_latihan4_kambing;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis
+ * tipe data bilangan bulat 
+ */
+public class PBOIF2_10119062_Latihan4_KAMBING {
+    
+     public void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+       
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+    }
+     
+    public static void main(String[] args) {
+        PBOIF2_10119062_Latihan4_KAMBING kambingJantan = new 
+        PBOIF2_10119062_Latihan4_KAMBING();
+        kambingJantan.tambahKambing();
+    }
+}    
+
